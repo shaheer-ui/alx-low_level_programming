6fecc8e57572f9bc9f744c4e1d7bfd49f634e9ad
shaheer-ui <abdulsalshahir01@yahoo.com>
2022-09-08 13:51:37 -0700

    g

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
-* main - Entry point 
-* 
-* Return: 0 after printing the function 
-*/
+ * main - Entry point 
+ * 
+ * Return: 0 after printing the function 
+ */
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
-return (0);
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }
