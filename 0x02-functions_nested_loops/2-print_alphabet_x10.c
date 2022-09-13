99475a74a52c482120e9c8a4f04c0bc4f6529fcf
shaheer-ui <abdulsalshahir01@yahoo.com>
2022-09-13 07:01:01 -0700

    t2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,12 +10,13 @@ void print_alphabet_x10(void)
 	char c;
 	int i = 0;
 
-	while (i =< 9)
+	while (i <= 9)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
 			putchar(c);
 		}
-		putchar('\n')
+		putchar('\n');
+		
 	}
 }
