baa2d9002fe2f582184615e44b6b70ff5795b29f
shaheer-ui <abdulsalshahir01@yahoo.com>
2022-09-13 05:53:48 -0700

    ff

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
 
 int main(void)
 {
-	printf("_putchar\n");
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, sz;
+
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
 	return (0);
 }
 
