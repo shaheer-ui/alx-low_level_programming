a5c1c50a63ac7d42f7f10e1a49f110bb27ba9a76
shaheer-ui <abdulsalshahir01@yahoo.com>
2022-09-13 06:34:04 -0700

    t1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * print_alphabet - Entry point
+ * Description: a function that prints the alphabet,
+ * in lowercase followed by a new line
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+		putchar ('\n');
+}
+
+
