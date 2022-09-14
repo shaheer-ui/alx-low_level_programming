3961e58d97873bedd1902545a14aed061363092a
shaheer-ui <abdulsalshahir01@yahoo.com>
2022-09-14 12:56:49 -0700

    dd

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -24,15 +24,13 @@ void print_times_table(int n)
 						_putchar(' ');
 					}
 					_putchar(mult + '0');
-				}
-				else if (mult < 100)
+				} else if (mult < 100)
 				{
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((mult / 10) + '0');
 					_putchar((mult % 10) + '0');
-				}
-				else
+				} else
 				{
 					_putchar(' ');
 					_putchar((mult / 100) + '0');
