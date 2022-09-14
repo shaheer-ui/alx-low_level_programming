496b10dd21171a5bddc1dd488d5c0251e27c2166
shaheer-ui <abdulsalshahir01@yahoo.com>
2022-09-14 12:54:07 -0700

    dd

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * print_times_table - prints times table from input
+ * @n: input (must be bet 1 and 15)
+ * @n: input (must be bet 1 and 15)
+ */
+void print_times_table(int n)
+{
+	int i, j, mult;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				mult = i * j;
+				if (mult < 10)
+				{
+					if (j != 0)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+					}
+					_putchar(mult + '0');
+				}
+				else if (mult < 100)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((mult / 10) + '0');
+					_putchar((mult % 10) + '0');
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar((mult / 100) + '0');
+					_putchar(((mult % 100) / 10) + '0');
+					_putchar((mult % 10) + '0');
+				}
+				if (j != n)
+				{
+					_putchar(',');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
